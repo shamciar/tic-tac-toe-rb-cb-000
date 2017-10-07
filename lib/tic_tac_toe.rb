3d5cbf699053d2b2765a61682b1ece0df27c4b9c
Jake Matteson <jake.matteson@gmail.com>
2017-10-07 22:20:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,3 +74,22 @@ def winner(board)
     end
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space != " "
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  if turn % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
